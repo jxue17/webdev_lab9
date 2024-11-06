a07480717371172b383df802fbb9a7971c56f1c5
jxue17 <jxue17@hawk.iit.edu>
2024-11-05 23:24:43 -0600

    finished text function

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 function init(){
 //add your javascrip between these two lines of code
   var button = document.getElementById("entrybutton");
-  function Text()
+  function text()
   {
-    
-
-
-
-
-
+    var textbox = document.getElementById("entryinput");
+    document.getElementById("textoutput").innerHTML = textbox.value;
+    alert("Jianfeng Xue: " + textbox.value);
+  }
+  button.addEventListener("click", text);
+}
 window.addEventListener('load', init);
